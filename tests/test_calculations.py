a641b2e6b1077d954e3e5d64bd0a20cd14815e5a
adamabdell <adam.abd13@gmail.com>
2022-07-22 11:33:31 -0400

    name changes, put zakat percentage in variable

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,22 +1,22 @@
 import pytest
 from src.services import calculations
 
-def test_zakat_owed_per_year_should_return_two_point_fifty_seven_percent_of_amount():
+def test_zakat_owed_per_year_should_return_correct_amount():
 
     amount = 100.0
 
-    service = calculations.CalculateFundLifetime
+    service = calculations.CalculateFundLifetimeService
     zakat_owed = service.zakat_owed_per_year(amount)
 
     assert zakat_owed == 2.57
 
 
-def test_calculate_wealth_after_stock_growth():
+def test_calculate_wealth_after_stock_growth_should_return_correct_amount():
 
     amount = 100.50
     growth_percentage = 7
 
-    service = calculations.CalculateFundLifetime
+    service = calculations.CalculateFundLifetimeService
     amount_after_growth = service.calculate_wealth_after_stock_growth(amount, growth_percentage)
 
     assert amount_after_growth == 107.54
